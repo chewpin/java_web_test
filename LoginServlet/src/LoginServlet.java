6cceda8bb4ec56cb1b9f07d6f62a943cf89d64aa
chewpin <jessicachen@JessicadeMacBook-Pro.local>
2015-10-05 22:13:23 -0400

    form

--- LoginServlet/src/LoginServlet.java
+++ LoginServlet/src/LoginServlet.java
@@ -62,26 +62,48 @@ public class LoginServlet extends HttpServlet {
 //		// TODO Auto-generated method stub
 //	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
-        //get request parameters for userID and password
-        String user = request.getParameter("user");
-        String pwd = request.getParameter("pwd");
-         
-        //get servlet config init params
-        String userID = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        //logging example
-        log("User="+user+"::password="+pwd);
-         
-        if(userID.equals(user) && password.equals(pwd)){
-            response.sendRedirect("LoginSuccess.jsp");
-        }else{
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out= response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include(request, response);
-             
-        }
+		if(request.getParameter("user")!=null) {
+	        //get request parameters for userID and password
+	        String user = request.getParameter("user");
+	        String pwd = request.getParameter("pwd");
+	         
+	        //get servlet config init params
+	        String userID = getServletConfig().getInitParameter("user");
+	        String password = getServletConfig().getInitParameter("password");
+	        //logging example
+	        log("User="+user+"::password="+pwd);
+	         
+	        if(userID.equals(user) && password.equals(pwd)){
+	            response.sendRedirect("LoginSuccess.jsp");
+	        }else{
+	            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+	            PrintWriter out= response.getWriter();
+	            out.println("<font color=red>Either user name or password is wrong.</font>");
+	            rd.include(request, response);
+	             
+	        }
+		}
+		else if(request.getParameter("url")!=null) {
+	        //get request parameters for userID and password
+	        String url = request.getParameter("url");
+	        String date_time = request.getParameter("date_time");
+	         
+	        //logging example
+	        log("url="+url+"::date_time="+date_time);
+	        
+	        request.setAttribute("url", url);
+	        request.setAttribute("date_time", date_time);
+	        //Servlet JSP communication
+			RequestDispatcher reqDispatcher = getServletConfig().getServletContext().getRequestDispatcher("/jsps/hello.jsp");
+			reqDispatcher.forward(request,response);
+	        
+	         
+	        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+	        PrintWriter out= response.getWriter();
+	        out.println("<font color=red> url: " + url + ", date_time: " + date_time + ".</font>");
+	        rd.include(request, response);
+	           
+		}
          
     }
 
